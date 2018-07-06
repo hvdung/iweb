40c6f0440cdf732ebbc50b5191edfb886181d08a
unknown <hvdung14@gmail.com>
2018-07-06 15:57:33 +0700

    Mới làm Post thôi nên k có j đâu

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:edit, :update, :show, :delete]
+  before_action :find_post, only: [:edit, :update, :show, :destroy]
 
   def index
     @posts = Post.all
