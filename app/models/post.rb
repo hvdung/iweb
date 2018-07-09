196e38e588f9bd0d4d0ea4708d19bcd6659000ae
unknown <hvdung14@gmail.com>
2018-07-09 11:36:17 +0700

    Show post error message

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   mount_uploader :thumbnail, ThumbnailUploader
   validates :title, presence: true
+  validates :desc, length: { maximum: 250 }
 end
