0a235066797e6c214066db04d21397cf0c876ebd
unknown <hvdung14@gmail.com>
2018-07-10 16:41:06 +0700

    Authentication with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   get 'pages/home'
 
   devise_for :users
   mount Ckeditor::Engine => '/ckeditor'
   root 'pages#home'
-
+  
+  resources :users, only: [:index]
   resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
