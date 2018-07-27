2f779a325f5db7b2ceb606852cad6e34bf2dc9b0
unknown <hvdung14@gmail.com>
2018-07-27 11:57:48 +0700

    Filter multiple options - fix scope

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,15 +6,20 @@ class PostsController < ApplicationController
     @categories = Category.all
     @users = User.all
 
-    if params[:cat_id].present?
-      @posts = Post.joins(:relationships).where("category_id = ?", params[:cat_id]).paginate(:page => params[:page], :per_page => 16)
-    elsif params[:user_id].present?
-      @posts = Post.all.where("user_id = ?", params[:user_id]).paginate(:page => params[:page], :per_page => 16)
-    elsif params[:cat_id].present? and params[:user_id].present?
-      @posts = Post.joins(:relationships).where("category_id = :cat_id and user_id = :user_id", {cat_id: params[:cat_id], user_id: params[:user_id]}).paginate(:page => params[:page], :per_page => 16)
-    else
-      @posts = Post.paginate(:page => params[:page], :per_page => 16)
-    end
+    # if params[:cat_id].present? and params[:user_id].present?
+    #   @posts = Post.joins(:relationships).where("category_id = :cat_id and user_id = :user_id", {cat_id: params[:cat_id], user_id: params[:user_id]}).paginate(:page => params[:page], :per_page => 16)
+    # elsif params[:user_id].present?
+    #   @posts = Post.all.where("user_id = ?", params[:user_id]).paginate(:page => params[:page], :per_page => 16)
+    # elsif params[:cat_id].present?
+    #   @posts = Post.joins(:relationships).where("category_id = ?", params[:cat_id]).paginate(:page => params[:page], :per_page => 16)
+    # else
+    #   @posts = Post.paginate(:page => params[:page], :per_page => 16)
+    # end
+
+    @posts = Post.where(nil)
+    @posts = @posts.cat_id(params[:cat_id]) if params[:cat_id].present?
+    @posts = @posts.user_id(params[:user_id]) if params[:user_id].present?
+    @posts = @posts.paginate(page: params[:page], per_page: 16)
 
   end
 
